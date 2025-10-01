53fd2c4878e9fe8343a31a415d98a112cfb3f08b
quentinRolld <82177625+quentinRolld@users.noreply.github.com>
2025-10-01 13:30:32 +0000

    Deploying to gh-pages from @ quentinRolld/quentinRolld.github.io@b32d0426bf59bfc9e12ecc332bb2af8a36644b16 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -456,9 +456,9 @@ ninja.data = [{
           description: "automous car race",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-mini-apteros",
+          title: 'mini-apteros',
+          description: "autonomous reusable launcher",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
             },},{id: "projects-project-3-with-very-long-name",
@@ -466,19 +466,19 @@ ninja.data = [{
           description: "a project that redirects to another website",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-the-education-project",
+          title: 'The education project',
+          description: "Creation of an autonomous robot with memory",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-walle",
+          title: 'Walle-',
+          description: "A robot to tidy my bedroom",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-cansat",
+          title: 'Cansat',
+          description: "automous flying robot",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
             },},{id: "projects-project-7",
