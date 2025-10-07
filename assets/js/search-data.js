e393d9173ec84b6aef45608840a8b33a2d75005a
quentinRolld <82177625+quentinRolld@users.noreply.github.com>
2025-10-07 20:57:25 +0000

    Deploying to gh-pages from @ quentinRolld/quentinRolld.github.io@78b0f2c71653274f0a85bc85d2fec65754217e31 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -263,35 +263,21 @@ ninja.data = [{
         title: 'email',
         section: 'Socials',
         handler: () => {
-          window.open("mailto:%79%6F%75@%65%78%61%6D%70%6C%65.%63%6F%6D", "_blank");
+          window.open("mailto:%71%75%65%6E%74%69%6E.%72%6F%6C%6C%61%6E%64%32@%63%65%61.%66%72", "_blank");
         },
       },{
-        id: 'social-inspire',
-        title: 'Inspire HEP',
+        id: 'social-ieee',
+        title: 'IEEE Xplore',
         section: 'Socials',
         handler: () => {
-          window.open("https://inspirehep.net/authors/1010907", "_blank");
+          window.open("https://ieeexplore.ieee.org/author/478922/", "_blank");
         },
       },{
-        id: 'social-rss',
-        title: 'RSS Feed',
+        id: 'social-linkedin',
+        title: 'LinkedIn',
         section: 'Socials',
         handler: () => {
-          window.open("/feed.xml", "_blank");
-        },
-      },{
-        id: 'social-scholar',
-        title: 'Google Scholar',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://scholar.google.com/citations?user=qc6CJjYAAAAJ", "_blank");
-        },
-      },{
-        id: 'social-custom_social',
-        title: 'Custom_social',
-        section: 'Socials',
-        handler: () => {
-          window.open("https://www.alberteinstein.com/", "_blank");
+          window.open("https://www.linkedin.com/in/quentinrolland", "_blank");
         },
       },{
       id: 'light-theme',
